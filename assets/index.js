ebd5d8f1109164909a11e00ee20075bc8206d7fc
Ishan <ishanwijes@gmail.com>
2022-07-04 17:35:50 +1000

    finished carousel component

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,10 @@
 let swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
+    slidesPerView: 4, // how many images can you see in the viewport at once in the carousel
+    spaceBetween: 20, // in pixels
+
+    // this is to make the arrow buttons scroll by however many images you want
+    // slidesPerGroup: 2,
+
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
